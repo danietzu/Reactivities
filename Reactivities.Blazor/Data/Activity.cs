f2bb9aa3f31c1fd5b75641d461f71b54a75fac7f
Daniele Cerro <danielecerro92@gmail.com>
2020-05-18 19:38:02 +0200

    Improved DateTime Picker

--- Reactivities.Blazor/Data/Activity.cs
+++ Reactivities.Blazor/Data/Activity.cs
@@ -1,16 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Reactivities.Blazor.Data
 {
     public class Activity
     {
         public string Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
 
+        [Required]
         public string Description { get; set; }
+
+        [Required]
         public string Category { get; set; }
-        public DateTime Date { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [Required]
+        public DateTime Time { get; set; }
+
+        [Required]
         public string City { get; set; }
+
+        [Required]
         public string Venue { get; set; }
+
+        public static List<string> Categories
+        {
+            get => new List<string> { "Drinks", "Culture", "Film", "Food", "Music", "Travel" };
+        }
     }
 }
